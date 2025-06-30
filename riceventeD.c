db544ff53520187a3f60ee29b1b907812b6c60fa
Giuseppe Bencivenga <164526707+pepbcv@users.noreply.github.com>
2025-06-30 15:28:42 +0200

    Update riceventeD.c

--- riceventeD.c
+++ riceventeD.c
@@ -21,12 +21,14 @@ int main(int argc, char ** argv){
     domid = strtoul(argv[1], NULL, 10);
     refid[0] = strtoul(argv[2], NULL, 10);
 
+    //aprire file descriptor sul nodo gntdev
     int gntdev_fd = open("/dev/xen/gntdev", O_RDWR);
     if(gntdev_fd < 0){
         fprintf(stderr, "Couldn't open /dev/xen/gntdev\n");
         exit(EXIT_FAILURE);
     }
 
+    //allocazione struttura che serve a comunicare al kernel quali grant vogliamo mappare (allochaimo nb_grant elementi - 1, perchè perche la struct già ne include uno) 
     struct ioctl_gntdev_map_grant_ref* gref = malloc(sizeof(struct ioctl_gntdev_map_grant_ref) + (nb_grant-1) * sizeof(struct ioctl_gntdev_grant_ref));
     if(gref == NULL){
         fprintf(stderr, "Couldn't allocate struct ioctl_gntdev_map_grant_ref\n");
@@ -34,6 +36,7 @@ int main(int argc, char ** argv){
         exit(EXIT_FAILURE);
     }
 
+    //popolamento con i grant ricevuti dal mittente
     gref->count = nb_grant;
     for(i = 0; i < nb_grant; i++){
         struct ioctl_gntdev_grant_ref* ref = &gref->refs[i];
@@ -41,6 +44,7 @@ int main(int argc, char ** argv){
         ref->ref = refid[i];
     }
 
+    //chiamata al driver per far mappare pagine esterne nel proprio processo
     int err = ioctl(gntdev_fd, IOCTL_GNTDEV_MAP_GRANT_REF, gref);
     if(err < 0){
         fprintf(stderr, "IOCTL failed\n");
@@ -49,6 +53,7 @@ int main(int argc, char ** argv){
         exit(EXIT_FAILURE);
     }
 
+    //ottenimento di un puntatore userspace per avere accesso alla memoria lato domU
     char* shbuf = mmap(NULL, nb_grant*PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, gntdev_fd, gref->index);
     if(shbuf == MAP_FAILED){
         fprintf(stderr, "Failed mapping the grant references\n");
