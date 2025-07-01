433026681dd57581d82c2b1aa7e682caf2bf6336
Giuseppe Bencivenga <164526707+pepbcv@users.noreply.github.com>
2025-07-01 16:24:05 +0200

    Update r2.c

--- r2.c
+++ r2.c
@@ -64,8 +64,19 @@ int main(int argc, char ** argv){
     }
 
     //printf("%s\n", shbuf);
-    fwrite(shbuf, 1, 1024, stdout);
-    printf("\n");
+    //fwrite(shbuf, 1, 1024, stdout);
+    //printf("\n");
+    
+    for (int i = 0; i < 10; i++) {
+    // Aspetto che ci sia un messaggio da leggere
+    while (shbuf[0] != 1);
+
+    // Leggo il messaggio
+    printf("Ricevente ha ricevuto: %s\n", shbuf + 1);
+
+    // Resetto il flag per permettere al mittente di scrivere il prossimo
+    shbuf[0] = 0;
+    }
 
 
     err = munmap(shbuf, nb_grant*PAGE_SIZE);
