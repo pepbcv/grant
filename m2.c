050a9cf8eccd3bc2f0f6f676b39d51e396a1d624
Giuseppe Bencivenga <164526707+pepbcv@users.noreply.github.com>
2025-07-01 16:25:24 +0200

    Update m2.c

--- m2.c
+++ m2.c
@@ -8,70 +8,102 @@
 #include <sys/mman.h>
 #include <signal.h>
 
-#include <xen/gntalloc.h>
+#include <xen/gntalloc.h> //inclusione driver per funzionalià grant tables
 
 #define PAGE_SIZE getpagesize()
-#define MSG_SIZE 1023
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <receiver_domid>\n", argv[0]);
-        return 1;
-    }
+//passa domuB_ID 
+int main(int argc, char ** argv){
+    uint16_t domid = 3; //We set to share these grants with Dom0 (domID della vm a cui si vuole dare accesso, cioè domuB_id)
+    uint32_t count = 1; //We want to allocate one grant/page
 
-    uint16_t domid = strtoul(argv[1], NULL, 10);
-    uint32_t count = 1;
-
+    //apertura file descriptor sul nodo driver
     int gntalloc_fd = open("/dev/xen/gntalloc", O_RDWR);
-    if (gntalloc_fd < 0) {
-        perror("Couldn't open /dev/xen/gntalloc");
+    if(gntalloc_fd < 0){
+        fprintf(stderr, "Couldn't open /dev/xen/gntalloc\n");
         exit(EXIT_FAILURE);
     }
 
-    struct ioctl_gntalloc_alloc_gref *gref = malloc(sizeof(*gref));
-    gref->domid = domid;
-    gref->count = count;
+    //allocazione dinamica struttura che contiene i parametri necessari per la chiamata ioctl (che alloca grant references)
+    struct ioctl_gntalloc_alloc_gref* gref = malloc(sizeof(struct ioctl_gntalloc_alloc_gref));
+    if(gref == NULL){
+        fprintf(stderr, "Couldn't allocate struct ioctl_gntalloc_alloc_gref\n");
+        close(gntalloc_fd);
+        exit(EXIT_FAILURE);
+    }
+    //la struttura una volta allocata, viene popolata
+    gref->domid = domid; //domID di chi si vuole venga concessa la pagina
+    gref->count = count; //numero di grant references
     gref->flags = GNTALLOC_FLAG_WRITABLE;
 
-    if (ioctl(gntalloc_fd, IOCTL_GNTALLOC_ALLOC_GREF, gref) < 0) {
-        perror("IOCTL_ALLOC_GREF failed");
-        close(gntalloc_fd);
+    //Invio richiesta al driver gntalloc (con questo comando effettivamente si alloca la pagina e si ottiene il riferimento)
+    int err = ioctl(gntalloc_fd, IOCTL_GNTALLOC_ALLOC_GREF, gref);
+    if(err < 0){
+        fprintf(stderr, "IOCTL failed\n");
         free(gref);
+        close(gntalloc_fd);
         exit(EXIT_FAILURE);
     }
 
     printf("gref = %u\n", gref->gref_ids[0]);
 
-    char *shpages = mmap(NULL, count * PAGE_SIZE, PROT_READ | PROT_WRITE,
-                         MAP_SHARED, gntalloc_fd, gref->index);
-    if (shpages == MAP_FAILED) {
-        perror("mmap failed");
-        close(gntalloc_fd);
+    //chiamata map: mappa la memoria tra kernel e spazio utente, cioè con questo comando (a cui passo le info ottenute da ioctl) ottengo un riferimento utilizzabile dalla domU
+    char* shpages = mmap(NULL, count*PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, gntalloc_fd, gref->index);
+    if(shpages == MAP_FAILED){
+        fprintf(stderr, "mapping the grants failed\n");
         free(gref);
+        close(gntalloc_fd);
         exit(EXIT_FAILURE);
     }
 
+    //sprintf(shpages, "Hello, World!");
+    //memset(shpages, 'A', 1023);
+    //shpages[1023] = '\0';
+    
     for (int i = 0; i < 10; i++) {
-        while (shpages[0] != 0) usleep(100);  // attende che B legga
+    // Aspetto che il ricevente abbia letto il messaggio precedente
+    while (shpages[0] != 0);
 
-        memset(shpages + 1, 'A' + (i % 26), MSG_SIZE);
-        shpages[MSG_SIZE + 1] = '\0';
-        shpages[0] = 1;  // notifica B
+    // Scrivo il messaggio
+    snprintf(shpages + 1, 1023, "Messaggio numero %d", i);
 
-        printf("Messaggio %d inviato\n", i + 1);
+    // Imposto il flag per notificare il ricevente
+    shpages[0] = 1;
+    printf("Mittente ha inviato: Messaggio numero %d\n", i);
+	}
+    
+    
+
+    /**
+     * We wait for interrupt to begin cleaning up
+     */
+    int sig;
+    sigset_t set;
+    sigemptyset(&set);
+    int ret = sigaddset(&set, SIGINT);
+    if(ret < 0){
+        fprintf(stderr, "Adding signal to sigset failed\n");
+    }
+    if(sigwait(&set, &sig)){
+        fprintf(stderr, "Sigwait failed with error\n");
     }
 
-    pause();  // aspetta CTRL+C per pulizia
+    err = munmap(shpages, count*PAGE_SIZE);
+    if(err < 0){
+        fprintf(stderr, "Unmapping grants failed\n");
+    }
 
-    munmap(shpages, count * PAGE_SIZE);
+    struct ioctl_gntalloc_dealloc_gref dgref;
+    dgref.index = gref->index;
+    dgref.count = count;
 
-    struct ioctl_gntalloc_dealloc_gref dgref = {
-        .index = gref->index,
-        .count = count
-    };
-    ioctl(gntalloc_fd, IOCTL_GNTALLOC_DEALLOC_GREF, &dgref);
+    err = ioctl(gntalloc_fd, IOCTL_GNTALLOC_DEALLOC_GREF, &dgref);
+    if(err < 0){
+        fprintf(stderr, "IOCTL for deallocating grants failed\n");
+    }
 
-    close(gntalloc_fd);
+
     free(gref);
-    return 0;
+    close(gntalloc_fd);
+    exit(EXIT_SUCCESS);
 }
