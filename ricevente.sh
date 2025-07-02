cc4aab1eda199a14e9402ad5e727839b37a085e8
Giuseppe Bencivenga <164526707+pepbcv@users.noreply.github.com>
2025-07-02 12:35:37 +0200

    Create ricevente.sh

---
+++ ricevente.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOMIDA=$1
+GREF=$2
+
+LOGFILE="ricevente_metrics.log"
+
+echo "Stato prima del test:" > "$LOGFILE"
+free -h >> "$LOGFILE"
+top -b -n1 | grep "Cpu(s)" >> "$LOGFILE"
+echo "-------------------------------" >> "$LOGFILE"
+
+echo "Avvio ricevente (r7) con domid = $DOMIDA e grant_ref = $GREF..."
+sudo ./r7 "$DOMIDA" "$GREF"
+
+echo "-------------------------------" >> "$LOGFILE"
+echo "Stato dopo il test:" >> "$LOGFILE"
+free -h >> "$LOGFILE"
+top -b -n1 | grep "Cpu(s)" >> "$LOGFILE"
+
+echo "Test completato. Risultati salvati in $LOGFILE"
