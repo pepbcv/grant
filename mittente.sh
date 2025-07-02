01d6220f62e41093049cd1f26ec1b6ec4d2b6ce3
Giuseppe Bencivenga <164526707+pepbcv@users.noreply.github.com>
2025-07-02 12:31:13 +0200

    Create mittente.sh

---
+++ mittente.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DOMID=$1
+LOGFILE="mittente_metrics.log"
+
+echo "=== STATO PRIMA DEL TEST ===" > "$LOGFILE"
+date >> "$LOGFILE"
+free -h >> "$LOGFILE"
+top -b -n1 | grep "Cpu(s)" >> "$LOGFILE"
+echo "-------------------------------" >> "$LOGFILE"
+
+START=$(date +%s.%N)
+./mittente "$DOMID" >> "$LOGFILE"
+END=$(date +%s.%N)
+
+echo "=== STATO DOPO IL TEST ===" >> "$LOGFILE"
+date >> "$LOGFILE"
+free -h >> "$LOGFILE"
+top -b -n1 | grep "Cpu(s)" >> "$LOGFILE"
+echo "Durata totale (sec): $(echo "$END - $START" | bc)" >> "$LOGFILE"
